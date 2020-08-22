f54f419adf9008653ecc1d94af4198e1fe6dc351
Rookie81 <archeromar@yahoo.com>
2020-08-22 08:06:13 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,49 +12,53 @@ from tensorflow.keras.layers import Convolution2D, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
 
 
-
 ############################################################
-def importdataset(path):
-    columns = ['Center', 'Left', 'Right','Steering', 'Throttle', 'Brake', 'Speed']
-    data = pd.read_csv(os.path.join(path, 'driving_log.csv'),names =columns)
-    print(data.head())
+def getName(filePath):
+    return filePath.split('\\')[-1]
+
+
+def importDataInfo(path):
+    columns = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Brake', 'Speed']
+    data = pd.read_csv(os.path.join(path, 'driving_log.csv'), names=columns)
+    #### REMOVE FILE PATH AND GET ONLY FILE NAME
+    # print(getName(data['center'][0]))
+    data['Center'] = data['Center'].apply(getName)
+    # print(data.head())
+    print('Total Images Imported', data.shape[0])
     return data
 
+
 ############################################################
 
-def DataBalancing(data,display = True):
-    nBins = 31
-    samplePerBin = 1500
-    histogram , bins =np.histogram(data['Steering'],nBins)
-    print(bins)
+def balanceData(data, display=True):
+    nBin = 31
+    samplesPerBin = 500
+    hist, bins = np.histogram(data['Steering'], nBin)
     if display:
-        center = (bins[:-1]+ bins[1:])*0.5
-        print(center)
-        plt.bar(center,histogram,width = 0.06)
-        plt.plot((-1,1),(samplePerBin,samplePerBin))
+        center = (bins[:-1] + bins[1:]) * 0.5
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
         plt.show()
-
-    removeIndexList = []
-    for j in range(nBins):
-        binDatalist =[]
+    removeindexList = []
+    for j in range(nBin):
+        binDataList = []
         for i in range(len(data['Steering'])):
-            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j+1]:
-                binDatalist.append(i)
-        binDatalist= shuffle(binDatalist)
-        binDatalist= binDatalist[samplePerBin:]
-        removeIndexList.extend(binDatalist)
-    print('Removed Images: ',len(removeIndexList))
-    data.drop(data.index[removeIndexList],inplace = True)
-    print('Remaining Images: ',len(data))
+            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j + 1]:
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[samplesPerBin:]
+        removeindexList.extend(binDataList)
 
-
+    print('Removed Images:', len(removeindexList))
+    data.drop(data.index[removeindexList], inplace=True)
+    print('Remaining Images:', len(data))
     if display:
-        histogram, _ = np.histogram(data['Steering'], nBins)
-        plt.bar(center,histogram,width = 0.06)
-        plt.plot((-1,1),(samplePerBin,samplePerBin))
+        hist, _ = np.histogram(data['Steering'], (nBin))
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
         plt.show()
-
     return data
+
 
 ########################################################
 def DataLoading(path,data):
@@ -70,66 +74,68 @@ def DataLoading(path,data):
     steering = np.asarray(steering)
     return imgpath,steering
 
+
 ############################################################
 
 
-def augment(imgpath,steering):
-    img = mpimg.imread(imgpath)
-
-
-    if np.random.rand()< 0.5:
-        pan = iaa.Affine(translate_percent={'x':(-0.1,0.1),'y':(-0.1,0.1)})
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
         img = pan.augment_image(img)
-    if np.random.rand()< 0.5:
-        zoom = iaa.Affine(scale=(1,1.2))
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
         img = zoom.augment_image(img)
-    if np.random.rand()< 0.5:
-        brightness = iaa.Multiply((0.4,1.2))
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.2, 1.2))
         img = brightness.augment_image(img)
-    if np.random.rand()< 0.5:
-        img = cv2.flip(img,1)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
         steering = -steering
+    return img, steering
 
 
-
-    return steering,img
-
-
-def preprocessing(img):
-    img = img[60:135,:,:]
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,(3,3),0)
-    img = cv2.resize(img,(200,66))
-    img = img/255
+def preProcess(img):
+    img = img[60:135, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
     return img
 
 
-def batchGen(imgpath,steeringList,batchsize,trainflag):
+def batchGen(imagesPath, steeringList, batchSize, trainFlag):
     while True:
-        imgbatch = []
-        steeringbatch = []
+        imgBatch = []
+        steeringBatch = []
 
-        for i in range(batchsize):
-            index=random.randint(0,len(imgpath)-1)
-            if trainflag:
-                img , steering = augment(imgpath[index],steeringList[index])
+        for i in range(batchSize):
+            index = random.randint(0, len(imagesPath) - 1)
+            if trainFlag:
+                img, steering = augmentImage(imagesPath[index], steeringList[index])
             else:
-                img = mpimg.imread(imgpath[index])
+                img = mpimg.imread(imagesPath[index])
                 steering = steeringList[index]
-                img = preprocessing(img)
-                imgbatch.append(img)
-                steeringbatch.append(steering)
-        yield (np.asarray(imgbatch),np.asarray(steeringbatch))
-
-def modelcreation:
-    model = Sequential
+            img = preProcess(img)
+            imgBatch.append(img)
+            steeringBatch.append(steering)
+        yield np.asarray(imgBatch), np.asarray(steeringBatch)
 
 
+def createModel():
+    model = Sequential()
 
+    model.add(Convolution2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='elu'))
+    model.add(Convolution2D(36, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(48, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
 
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
 
-
-
-
-
-
+    model.compile(Adam(lr=0.0001), loss='mse')
+    return model
